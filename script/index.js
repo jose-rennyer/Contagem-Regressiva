85704856caa37f55753837fe38e7d07ba92f9b00
jose-rennyer <joselimao2002@gmail.com>
2022-02-17 21:41:04 -0300

    Contagem regressiva 1.0 primeiro commit

---
+++ script/index.js
@@ -0,0 +1,91 @@
+let hora = document.querySelector("#campo1").value
+let min = document.querySelector("#campoprimario").value
+let sec = document.querySelector("#camposecundario").value
+let mil = 100
+let ateofim;
+
+function contar(){
+    hora = document.querySelector("#campo1").value
+    min = document.querySelector("#campoprimario").value
+    sec = document.querySelector("#camposecundario").value
+    mil = 100
+    if(min == 0 && sec == 0){
+        min
+        sec
+        mil
+    }
+    ateofim = setInterval(contando, 10)
+}
+function zero(digito){
+    if(digito < 10){
+        return digito = "0" + digito
+    } else{
+        return digito
+    }
+}
+
+function parar(){
+    clearInterval(ateofim)
+    hora = 0
+    min = 0
+    sec = 0
+    mil = 100
+    document.querySelector(".numero").innerHTML = `${zero(hora)}:${zero(min)}:${zero(sec)}`
+}
+
+/*function contando(){
+    mil--
+    if(hora == 0 & min == 0 && sec == 0){
+        clearTimeout(ateofim)
+        min == 0
+        sec == 0
+    } else if(mil == 0){
+        mil = 99
+        sec--
+    } else if(sec == 0 && min == 0 && hora == 0){
+        sec = 0
+    } else if(sec == 0){
+        sec = 60
+        min--
+    }else if (min == 0 && hora == 0){
+        sec-
+        min = 0
+    }else if(min == 0){
+        min = 59
+        hora--
+    } else if (hora < 0){
+        hora = 0
+    } 
+    document.querySelector(".numero").innerHTML = `${zero(hora)}:${zero(min)}:${zero(sec)}`
+}*/
+
+function contando(){
+    //primeira parte
+    /*if(mil == 0){
+        mil = 100
+        sec--
+    } else if(sec == 0){
+        sec = 59
+        min--
+    }*/
+    mil--
+    if(mil == 0){
+        mil = 100
+        sec--
+    } else if(sec == 0){
+        verificasec()
+    }
+    document.querySelector(".numero").innerHTML = `${zero(min)}:${zero(sec)}`
+}
+
+function verificasec(){
+    if(min == 0 && sec == 0){
+        sec = 0
+        clearTimeout(ateofim)
+    } else {
+        sec = 59
+        min--
+    }
+}
+
+
